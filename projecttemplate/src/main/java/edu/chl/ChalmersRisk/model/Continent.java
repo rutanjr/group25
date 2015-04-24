0d5820ff6a348709715a472f340792abc9190091
chrh <robin.seriousmail@gmail.com>
2015-04-24 08:57:01 +0200

    small revision of the continent class

--- projecttemplate/src/main/java/edu/chl/ChalmersRisk/model/Continent.java
+++ projecttemplate/src/main/java/edu/chl/ChalmersRisk/model/Continent.java
@@ -6,6 +6,8 @@ import java.util.List;
 /**
  * Created by rutanjr on 2015-03-31.
  * A class to represent continents. Controlling an entire continent grants bonus troops.
+ *
+ * @revisedBy Robin Jansson
  */
 public class Continent {
 
@@ -13,9 +15,7 @@ public class Continent {
     private int value; // this is used if we intend to have different Continents giving a different amount of bonus troops
     private List<Territory> territories = new ArrayList<Territory>();
 
-    //TODO territory references.
-
-    //player owns all contintent Bololean? Player? Where to check this?
+    // Constructor
 
     public Continent(String name, int value, List<Territory> territories)   {
         this.name = name;
@@ -29,6 +29,7 @@ public class Continent {
     public int getValue()   { return this.value; }
     public List<Territory> getTerritories() { return this.territories; }
 
+
     // Comand - Methods
 
     public void setName(String name){
@@ -39,10 +40,11 @@ public class Continent {
     * Compares owners of territories to see if they are all owned by the same player.
     *@param The Player object reference to the player who's tested if he controls all the
     * territories in the given continent.
+    * @return True of the same player owns all territories in a continent
     */
     public boolean isContinentOwned(Player player) {
         for (int i = territories.size(); i > 0; i--) {
-            if (!territories.get(i).getOwner().equals(player)) {
+            if (!territories.get( (i-1 ) ).getOwner().equals(player)) {
                 return false;
             }
         }
