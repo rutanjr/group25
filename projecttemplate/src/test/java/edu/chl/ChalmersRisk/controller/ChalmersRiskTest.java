b0d85820082dad8a044987fad649d9adc9799ea4
bbjorn <bbjorn@student.chalmers.se>
2015-04-30 09:59:17 +0200

    Added more tests to testTerritoriesAreConnected.

--- projecttemplate/src/test/java/edu/chl/ChalmersRisk/controller/ChalmersRiskTest.java
+++ projecttemplate/src/test/java/edu/chl/ChalmersRisk/controller/ChalmersRiskTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Created by Björn Bergqvist on 28/04/15.
@@ -25,6 +26,18 @@ public class ChalmersRiskTest {
         Territory testTer1 = new Territory("Test1",testContinent,testPlayer1);
         Territory testTer2 = new Territory("Test1",testContinent,testPlayer1);
 
-        assertTrue(ChalmersRisk.territoriesAreConnected(testTer1,testTer2,testPlayer1));
+        //Test when no territories are connected.
+        assertFalse(ChalmersRisk.territoriesAreConnected(testTer1, testTer2, testPlayer1));
+
+        ArrayList<Territory> adjTers = new ArrayList<Territory>();
+        adjTers.add(testTer2);
+        testTer1.addAdjacent(adjTers);
+
+        //Test when two territories are connected.
+        assertTrue(ChalmersRisk.territoriesAreConnected(testTer1, testTer2, testPlayer1));
+
+        //Test with a chain of connected territories.
+
+        //Test with a chain that includes a loop. 
     }
 }
