dc2a2f58bc9d766c06dfd302be174f12fdd1c2f4
bbjorn <bbjorn@student.chalmers.se>
2015-04-30 14:26:16 +0200

    Implemented test for combat - can't test two instances of Chalmers Risk in the same class.

--- projecttemplate/src/test/java/edu/chl/ChalmersRisk/controller/ChalmersRiskTest.java
+++ projecttemplate/src/test/java/edu/chl/ChalmersRisk/controller/ChalmersRiskTest.java
@@ -3,13 +3,15 @@ package edu.chl.ChalmersRisk.controller;
 import edu.chl.ChalmersRisk.model.Continent;
 import edu.chl.ChalmersRisk.model.Player;
 import edu.chl.ChalmersRisk.model.Territory;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
-
+import static org.junit.Assert.*;
 /**
  * Created by Björn Bergqvist on 28/04/15.
  */
@@ -118,16 +120,21 @@ public class ChalmersRiskTest {
         assertFalse(testRisk.moveTroops(testTer1, testTer2, -3));
     }
 
-    @Test 
-    public void testCombat(){
-        ChalmersRisk testRisk = new ChalmersRisk();
+    @Test (expected = IllegalArgumentException.class)
+    public void testCombatThrowsExceptionWhenTooFewTroops(){
+
+        ChalmersRisk testRisk2 = new ChalmersRisk();
         ArrayList<Territory> territories = new ArrayList<Territory>();
         Continent testContinent = new Continent("test",1,territories);
         Player testPlayer1 = new Player("1");
         Player testPlayer2 = new Player("2");
 
         Territory testTer1 = new Territory("Test1",testContinent,testPlayer1);
-        Territory testTer2 = new Territory("Test2",testContinent,testPlayer2)  
-        
+        Territory testTer2 = new Territory("Test2",testContinent,testPlayer2);
+
+        testTer1.addTroops(1);
+        testTer2.addTroops(1);
+
+        testRisk2.combat(testTer1,testTer2);
     }
 }
