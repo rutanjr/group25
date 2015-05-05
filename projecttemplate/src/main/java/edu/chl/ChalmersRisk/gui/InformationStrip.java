09594f89e4362afd7fd553ef3b9fc4500ec4d093
malinsan <malin.thelin12@gmail.com>
2015-05-05 13:55:12 +0200

    Change informationstrip to a borderpane instead

--- projecttemplate/src/main/java/edu/chl/ChalmersRisk/gui/InformationStrip.java
+++ projecttemplate/src/main/java/edu/chl/ChalmersRisk/gui/InformationStrip.java
@@ -2,13 +2,13 @@ package edu.chl.ChalmersRisk.gui;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 
 /**
  * Created by Malin on 2015-05-05.
  */
-public class InformationStrip extends HBox {
+public class InformationStrip extends BorderPane {
 
     Text gameText;
     Button nextButton;
@@ -18,14 +18,22 @@ public class InformationStrip extends HBox {
         nextButton = new Button("NEXT");
         nextButton.setPrefSize(100,50);
         this.setPadding(new Insets(15, 12, 15, 12));
-        this.setSpacing(200);
         this.setStyle("-fx-background-color: #336699;");
-        this.getChildren().addAll(gameText,nextButton);
+
+        this.setLeft(gameText);
+        this.setRight(nextButton);
+
+
+
     }
 
 
     public Text getGameText(){
         return gameText;
+    }
+
+    public void setGameText(String text){
+        gameText.setText(text);
     }
 
     public Button getNextButton(){
