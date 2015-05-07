019defcbda73ecc45b79fda454b7bdbaf8598a04
chrh <robin.seriousmail@gmail.com>
2015-05-07 15:37:58 +0200

    late revision of ICard

--- projecttemplate/src/main/java/edu/chl/ChalmersRisk/ICard.java
+++ projecttemplate/src/main/java/edu/chl/ChalmersRisk/ICard.java
@@ -6,13 +6,13 @@ package edu.chl.ChalmersRisk;
  */
 
 public interface ICard {
-    /*
-    // the title of the card
-    private String title() {}
 
-    public String getTitle() {}
+    // the title of the card
+    public String title();
+
+    public String getTitle();
 
     //turns the card, revealing its effect
-    public void turnCard() {}
-    */
+    public void turnCard();
+
 }
