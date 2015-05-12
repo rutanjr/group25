5f140cd084b91410d04797708d7aee0cd7340f39
chrh <robin.seriousmail@gmail.com>
2015-05-12 11:56:14 +0200

    added a getMessage in the ICard interface to include a message to the user who get the card

--- projecttemplate/src/main/java/edu/chl/ChalmersRisk/cardModels/ICard.java
+++ projecttemplate/src/main/java/edu/chl/ChalmersRisk/cardModels/ICard.java
@@ -10,6 +10,9 @@ public interface ICard {
     // returns the title of the card
     public String getTitle();
 
+    //returns the message of the card
+    public String getMessage();
+
     //turns the card, revealing its effect
     public void turnCard();
 
