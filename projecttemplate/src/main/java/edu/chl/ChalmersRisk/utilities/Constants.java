3aa1d353b16feb02f5a5fff6c8a3f0aef73fb750
malinsan <malin.thelin12@gmail.com>
2015-05-20 21:55:49 +0200

    Added constants EMPTY_TERRITORY and EMPTY_CONTINENT

--- projecttemplate/src/main/java/edu/chl/ChalmersRisk/utilities/Constants.java
+++ projecttemplate/src/main/java/edu/chl/ChalmersRisk/utilities/Constants.java
@@ -1,6 +1,8 @@
 package edu.chl.ChalmersRisk.utilities;
 
+import edu.chl.ChalmersRisk.model.Continent;
 import edu.chl.ChalmersRisk.model.Player;
+import edu.chl.ChalmersRisk.model.Territory;
 
 /**
  * Created by Malin on 2015-04-22.
@@ -12,5 +14,7 @@ public class Constants {
     public static final Player EMPTY_PLAYER = new Player("EMPTY","FFFFFF");
     public static final int width = 500;
     public static final int height = 500;
+    public static final Continent EMPTY_CONTINENT = new Continent("EMPTY",0);
+    public static final Territory EMPTY_TERRITORY = new Territory("EMPTY");
 
 }
