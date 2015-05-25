909d792337c892d70e03edd299bd3e4125e195e1
bbjorn <bbjorn@student.chalmers.se>
2015-05-25 15:35:52 +0200

    Created test class for AdditionalMoveCard.

---
+++ projecttemplate/src/test/java/edu/chl/ChalmersRisk/cardModels/AdditionalMoveCardTest.java
@@ -0,0 +1,15 @@
+package edu.chl.ChalmersRisk.cardModels;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by viking on 25/05/15.
+ */
+public class AdditionalMoveCardTest {
+    @Test
+    public void testTurnCard() throws Exception {
+        //TODO Write proper test.
+    }
+}
