4a5a06b78c425693a16598164e3385513f0b6835
chrh <robin.seriousmail@gmail.com>
2015-05-26 10:45:22 +0200

    added a blank card to have a more correct abstraction of deck

---
+++ projecttemplate/src/main/java/edu/chl/ChalmersRisk/cardModels/BlankCard.java
@@ -0,0 +1,36 @@
+package edu.chl.ChalmersRisk.cardModels;
+
+/**
+ * A blank card that wont have any effect on the game.
+ *
+ * Created by chrh on 26-May-15.
+ */
+public class BlankCard implements ICard {
+
+    private String title, message;
+
+    public BlankCard() {
+        this.title = "No effect";
+        this.message = "Nothing out of the ordinary occurs.";
+    }
+
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+
+    @Override
+    public int phaseCheck() {
+        return 0;
+    }
+
+    @Override
+    public void turnCard() {
+        // This card has no effect.
+    }
+}
